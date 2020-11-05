ab6d6564a68914fd63778a213a0afccf44a4fe4e
Edward <edward161514@gmail.com>
2020-11-05 03:16:13 -0600

    Added basic database

---
+++ cookiepassion.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2020 at 10:14 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cookiepassion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cookie`
+--
+
+CREATE TABLE `cookie` (
+  `name` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` varchar(50) NOT NULL DEFAULT '0.00',
+  `inventory` int(11) NOT NULL DEFAULT 0,
+  `ingredients` text DEFAULT NULL,
+  `del` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cookie`
+--
+
+INSERT INTO `cookie` (`name`, `description`, `price`, `inventory`, `ingredients`, `del`) VALUES
+('CHOCOLATE CHIP', 'Signature chocolate chip.', '1.50', 100, 'Chocolate', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetail`
+--
+
+CREATE TABLE `orderdetail` (
+  `id` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `cookieName` varchar(50) NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT 0,
+  `del` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orderdetail`
+--
+
+INSERT INTO `orderdetail` (`id`, `orderId`, `cookieName`, `amount`, `del`) VALUES
+(1, 1, 'CHOCOLATE CHIP', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `privilege` varchar(50) NOT NULL DEFAULT 'user',
+  `del` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `email`, `name`, `password`, `privilege`, `del`) VALUES
+('admin', 'admin@admin.com', 'admin', 'admin', 'admin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userorder`
+--
+
+CREATE TABLE `userorder` (
+  `username` varchar(50) NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `total` varchar(50) NOT NULL DEFAULT '0.00',
+  `orderTime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `del` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userorder`
+--
+
+INSERT INTO `userorder` (`username`, `orderId`, `total`, `orderTime`, `del`) VALUES
+('admin', 1, '1.50', '2020-11-05 09:10:31', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cookie`
+--
+ALTER TABLE `cookie`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orderIDFK` (`orderId`),
+  ADD KEY `cookieNameFK` (`cookieName`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `userorder`
+--
+ALTER TABLE `userorder`
+  ADD PRIMARY KEY (`orderId`),
+  ADD KEY `usernameFK` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `userorder`
+--
+ALTER TABLE `userorder`
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD CONSTRAINT `cookieNameFK` FOREIGN KEY (`cookieName`) REFERENCES `cookie` (`name`),
+  ADD CONSTRAINT `orderIDFK` FOREIGN KEY (`orderId`) REFERENCES `userorder` (`orderId`);
+
+--
+-- Constraints for table `userorder`
+--
+ALTER TABLE `userorder`
+  ADD CONSTRAINT `usernameFK` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
