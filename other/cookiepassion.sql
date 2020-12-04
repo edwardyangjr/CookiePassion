752bfa836cf8ef8ab8de43aed47715b29b0bf7c3
Edward <edward161514@gmail.com>
2020-12-03 22:59:14 -0600

    update DB

--- other/cookiepassion.sql
+++ other/cookiepassion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2020 at 01:24 AM
+-- Generation Time: Dec 04, 2020 at 05:53 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cookie` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `description` text DEFAULT NULL,
   `price` varchar(50) NOT NULL DEFAULT '0.00',
@@ -41,22 +42,25 @@ CREATE TABLE `cookie` (
 -- Dumping data for table `cookie`
 --
 
-INSERT INTO `cookie` (`name`, `description`, `price`, `inventory`, `ingredients`, `imageLocation`, `del`) VALUES
-('Biscuit', NULL, '1.60', 50, NULL, 'Images/biscuit.png', 1),
-('Caramel Chocolate Pecan Cookie', NULL, '1.99', 0, NULL, 'images/CaramelChocolatePecanCookie.png', 1),
-('Chocolate Chip / Peanut Butter', NULL, '1.59', 100, NULL, 'Images/chocolate_chip_peanut_butter_cookie.png', 1),
-('Chocolate Chip Cookie', 'Signature chocolate chip.', '1.50', 100, 'Chocolate', 'images/floating_chocolate_chip_cookies.png', 1),
-('Chocolate Chocolate Chip', NULL, '1.80', 100, NULL, 'images/ChocolateChocolateChip.png', 1),
-('Cookies N Cream', 'Made with Oreo.', '2.60', 100, 'Cream', 'images/CookiesNCream.png', 1),
-('Iced Sugar Cookie', 'Normal sugar cookie.', '1.20', 100, NULL, 'images/IcedSugarCookie.png', 1),
-('Jam Cookie', NULL, '2.10', 100, NULL, 'images/CaramelApple.png', 1),
-('Jelly Cookie', NULL, '3.30', 100, NULL, 'images/Jelly.png', 1),
-('Kisses Cookie', NULL, '5.50', 100, NULL, 'images/KissesCookie.png', 1),
-('Matcha Green Tea', NULL, '2.50', 100, NULL, 'images/MatchaGreenTea.png', 1),
-('Oatmeal Raisin', NULL, '1.79', 100, NULL, 'images/OatmealRaisin.png', 1),
-('Red Velvet Cookie', NULL, '3.50', 100, NULL, 'images/RedVelvet.png', 1),
-('Smores Cookie', NULL, '4.50', 100, NULL, 'images/Smores.png', 1),
-('Sugar Cookie', NULL, '0.99', 100, NULL, 'images/SugarCookie.png', 1);
+INSERT INTO `cookie` (`id`, `name`, `description`, `price`, `inventory`, `ingredients`, `imageLocation`, `del`) VALUES
+(1, 'Biscuit', 'Good', '1.61', 51, 'coco', 'images/cookies/biscuit.png', 1),
+(2, 'Caramel Chocolate Pecan Cookie', NULL, '1.99', 0, NULL, 'images/cookies/CaramelChocolatePecanCookie.png', 1),
+(3, 'Chocolate Chip / Peanut Butter', NULL, '1.59', 100, NULL, 'images/cookies/chocolate_chip_peanut_butter_cookie.png', 1),
+(4, 'Chocolate Chip Cookie', 'Signature chocolate chip.', '1.50', 100, 'Chocolate', 'images/cookies/floating_chocolate_chip_cookies.png', 1),
+(5, 'Chocolate Chocolate Chip', NULL, '1.80', 100, NULL, 'images/cookies/ChocolateChocolateChip.png', 1),
+(6, 'Cookies N Cream', 'Made with Oreo.', '2.60', 100, 'Cream', 'images/cookies/CookiesNCream.png', 1),
+(7, 'Iced Sugar Cookie', 'Normal sugar cookie.', '1.20', 100, NULL, 'images/cookies/IcedSugarCookie.png', 1),
+(8, 'Jam Cookie', NULL, '2.10', 100, NULL, 'images/cookies/CaramelApple.png', 1),
+(9, 'Jelly Cookie', NULL, '3.30', 100, NULL, 'images/cookies/Jelly.png', 1),
+(10, 'Kisses Cookie', NULL, '5.50', 100, NULL, 'images/cookies/KissesCookie.png', 1),
+(11, 'Matcha Green Tea', NULL, '2.50', 100, NULL, 'images/cookies/MatchaGreenTea.png', 1),
+(12, 'Oatmeal Raisin', NULL, '1.79', 100, NULL, 'images/cookies/OatmealRaisin.png', 1),
+(13, 'Red Velvet Cookie', NULL, '3.50', 100, NULL, 'images/cookies/RedVelvet.png', 1),
+(14, 'Smores Cookie', NULL, '4.50', 100, NULL, 'images/cookies/Smores.png', 1),
+(15, 'Sugar Cookie', NULL, '0.99', 100, NULL, 'images/cookies/SugarCookie.png', 1),
+(18, 'New Cookie', 'none', '8.00', 15, 'coco', '', 0),
+(19, 'cookiesadasd', '', '50.00', 20, '', '', 0),
+(20, 'Test', 'test', '5.00', 100, 'idk', '', 0);
 
 -- --------------------------------------------------------
 
@@ -67,7 +71,7 @@ INSERT INTO `cookie` (`name`, `description`, `price`, `inventory`, `ingredients`
 CREATE TABLE `orderdetail` (
   `id` int(11) NOT NULL,
   `orderId` int(11) NOT NULL,
-  `cookieName` varchar(50) NOT NULL,
+  `cookieID` int(11) NOT NULL,
   `amount` int(11) NOT NULL DEFAULT 0,
   `del` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -76,8 +80,8 @@ CREATE TABLE `orderdetail` (
 -- Dumping data for table `orderdetail`
 --
 
-INSERT INTO `orderdetail` (`id`, `orderId`, `cookieName`, `amount`, `del`) VALUES
-(3, 1, 'Chocolate Chip Cookie', 1, 1);
+INSERT INTO `orderdetail` (`id`, `orderId`, `cookieID`, `amount`, `del`) VALUES
+(3, 1, 4, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -88,8 +92,7 @@ INSERT INTO `orderdetail` (`id`, `orderId`, `cookieName`, `amount`, `del`) VALUE
 CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL,
   `privilege` varchar(50) NOT NULL DEFAULT 'user',
   `del` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -98,9 +101,10 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `email`, `name`, `password`, `privilege`, `del`) VALUES
-('admin', 'admin@admin.com', 'admin', 'admin', 'admin', 1),
-('user', 'user@user.com', 'user', 'user', 'user', 1);
+INSERT INTO `user` (`username`, `email`, `password`, `privilege`, `del`) VALUES
+('admin', 'admin@admin.com', 'admin', 'admin', 1),
+('tested', 'tested@gmail.com', '$2y$10$8zGKYUIW.cFuh30CMLOqfeXG6D5OQWolp15WTcOdXbesysdc5JMh6', 'user', 1),
+('user', 'user@user.com', 'user', 'user', 1);
 
 -- --------------------------------------------------------
 
@@ -121,7 +125,7 @@ CREATE TABLE `userorder` (
 --
 
 INSERT INTO `userorder` (`username`, `orderId`, `total`, `orderTime`, `del`) VALUES
-('admin', 1, '1.50', '2020-11-05 09:10:31', 1);
+('admin', 1, '1.50', '2020-11-05 15:10:31', 1);
 
 --
 -- Indexes for dumped tables
@@ -131,7 +135,7 @@ INSERT INTO `userorder` (`username`, `orderId`, `total`, `orderTime`, `del`) VAL
 -- Indexes for table `cookie`
 --
 ALTER TABLE `cookie`
-  ADD PRIMARY KEY (`name`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `orderdetail`
@@ -139,7 +143,7 @@ ALTER TABLE `cookie`
 ALTER TABLE `orderdetail`
   ADD PRIMARY KEY (`id`),
   ADD KEY `orderIDFK` (`orderId`),
-  ADD KEY `cookieNameFK` (`cookieName`);
+  ADD KEY `cookieIDFK` (`cookieID`);
 
 --
 -- Indexes for table `user`
@@ -157,6 +161,12 @@ ALTER TABLE `userorder`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cookie`
+--
+ALTER TABLE `cookie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `orderdetail`
@@ -178,7 +188,7 @@ ALTER TABLE `userorder`
 -- Constraints for table `orderdetail`
 --
 ALTER TABLE `orderdetail`
-  ADD CONSTRAINT `cookieNameFK` FOREIGN KEY (`cookieName`) REFERENCES `cookie` (`name`),
+  ADD CONSTRAINT `cookieIDFK` FOREIGN KEY (`cookieID`) REFERENCES `cookie` (`id`),
   ADD CONSTRAINT `orderIDFK` FOREIGN KEY (`orderId`) REFERENCES `userorder` (`orderId`);
 
 --
